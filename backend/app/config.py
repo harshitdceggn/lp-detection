755f6dd6892141aff83e767983bd860b62632c8c
Harshit Pandey <166249156+harshitdceggn@users.noreply.github.com>
2026-03-08 16:35:14 +0530

    Add files via upload

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,17 +1,17 @@
 import os
 
-# MODEL_PATH = os.getenv("MODEL_PATH", "model/best_3.onnx")
-# LABEL_PATH = os.getenv("LABEL_PATH", "model/label.txt")
-# import os
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-MODEL_PATH = os.path.join(BASE_DIR, "..", "model", "best_3.onnx")
-LABEL_PATH = os.path.join(BASE_DIR, "..", "model", "label.txt")
+MODEL_PATH = os.path.join(BASE_DIR, "model", "best_3.onnx")
+LABEL_PATH = os.path.join(BASE_DIR, "model", "label.txt")
+
 MODEL_W = 416
 MODEL_H = 416
+
 CONFIDENCE = 0.10
 NMS_THRESHOLD = 0.40
-CLOUD_NAME="dyb5z4ejf",
-API_KEY=597799715354648,
-API_SECRET="y7rVi2_LGkSeg_kQE0JdgsGpst8"
+
+CLOUD_NAME = os.getenv("CLOUD_NAME")
+API_KEY = os.getenv("API_KEY")
+API_SECRET = os.getenv("API_SECRET")
+DATABASE_URL = os.getenv("DATABASE_URL")

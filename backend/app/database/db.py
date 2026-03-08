6d5c4665c26972ddc19a0ee12a71c7489a1fc13e
Harshit Pandey <166249156+harshitdceggn@users.noreply.github.com>
2026-03-08 17:48:50 +0530

    Add files via upload

--- backend/app/database/db.py
+++ backend/app/database/db.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql://postgres:harshitP%40123@localhost:5432/lp_database"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
